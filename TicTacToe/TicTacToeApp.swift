a0346ca35f32afdc985b12b19bcf429c11600dc1
Hitesh Dalvi <hitesh.dalvi@fatakpay.com>
2024-05-07 18:04:30 +0530

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Hitesh Dalvi on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
